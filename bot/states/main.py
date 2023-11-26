8e271705a851a2597fdae836f3df41cc0ad29a8e
munwriter <glebvysokov3@gmail.com>
2023-11-26 19:23:45 +0500

    feat: add an interface for working with db

--- bot/states/main.py
+++ bot/states/main.py
@@ -3,7 +3,8 @@ from aiogram.fsm.state import StatesGroup, State
 
 class Currencies(StatesGroup):
     source_currency = State()
-    required_currency = State()
+    required_currencies = State()
+    rounding_idx = State()
 
 
 class Convert(StatesGroup):
